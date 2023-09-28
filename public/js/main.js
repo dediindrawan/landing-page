91041e59b273c6b04ed17699bf6ccd9c3063290a
Dedi Indrawan <dediindrawan812@gmail.com>
2023-09-29 00:24:09 +0700

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,33 @@
+'use strict';
+
+const data = 'public/data/data.json';
+
+const card = document.querySelector('.card');
+const faq = document.querySelector('.faq');
+
+const getDataObject = () => {
+    fetch(data)
+        .then(res => {
+            return res.json();
+        }).then(getData => {
+            card.innerHTML = '';
+            let listCard = getData.card;
+            listCard.forEach(list => {
+                card.innerHTML +=
+                    `
+                    <article class="text-center">
+                        <h1>${list.heading}</h1>
+                        <p class="mt-4 mb-6">${list.caption}</p>
+                        <img src="${list.image}" alt="image" class="w-4/5 mx-auto img-border">
+                    </article>
+                    `
+            });
+        }).catch(err => {
+            console.error(err);
+        });
+};
+document.addEventListener('DOMContentLoaded', getDataObject);
+
 const accordions = document.querySelectorAll('.accordions li:first-child');
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
@@ -18,3 +48,4 @@ accordions.forEach(accordion => {
         };
     });
 });
+
