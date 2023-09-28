c5c35adaf4fc6828ef2b32b8a61c363c4746a4b7
Dedi Indrawan <dediindrawan812@gmail.com>
2023-09-28 14:58:31 +0700

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,20 @@
-function createInput() {
-    const formInput = document.querySelector('#email-input');
-    formInput.focus();
-};
+const accordions = document.querySelectorAll('.accordions li:first-child');
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', () => {
+        accordions.forEach(accordion => {
+            if (accordion.nextElementSibling.style.display = 'inline-block') {
+                accordion.nextElementSibling.style.display = 'none';
+                accordion.firstElementChild.classList.remove('rotate-up-icon');
+            };
+        });
+        accordion.nextElementSibling.style.display = 'inline-block';
+        accordion.firstElementChild.classList.add('rotate-up-icon');
+    });
+    // click anywhere to close content
+    document.addEventListener('click', (e) => {
+        if (!accordion.contains(e.target)) {
+            accordion.nextElementSibling.style.display = 'none';
+            accordion.firstElementChild.classList.remove('rotate-up-icon');
+        };
+    });
+});
